85811219466b6863c92349c3d2a7ebd61505ca43
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-03-28 15:41:50 +0200

    remove halfway error list

--- java/src/test/java/kata/PasswordChecker.java
+++ java/src/test/java/kata/PasswordChecker.java
@@ -1,29 +1,22 @@
 package kata;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class PasswordChecker {
 
-    private List<String> errors = new ArrayList<String>();
-
     public boolean check(String password) {
-        errors.clear();
-        boolean result = password.length() >= 7 
+        return password.length() >= 7
                 && containsDigit(password)
                 && containsLetter(password);
-        if (!result) errors.add("oops");
-        return result;
     }
 
     public boolean checkAdmin(String password) {
-        errors.clear();
-        boolean result = password.length() >= 10
+        return password.length() >= 10
                 && check(password)
                 && containsSpecialCharacter(password);
-        if (!result) errors.add("oops");
-        return result;
     }
 
     private boolean containsSpecialCharacter(String password) {
@@ -36,10 +29,6 @@ public class PasswordChecker {
 
     private boolean containsDigit(String password) {
         return password.matches(".*\\d.*");
-    }
-
-    public List<String> errors() {
-        return errors;
     }
 
 }
--- java/src/test/java/kata/PasswordTest.java
+++ java/src/test/java/kata/PasswordTest.java
@@ -1,8 +1,8 @@
 package kata;
 
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 import static org.fest.assertions.api.Assertions.assertThat;
-import static org.junit.Assert.*;
-import junit.framework.Assert;
 
 import org.junit.Test;
 
@@ -11,68 +11,53 @@ public class PasswordTest {
 
     @Test public void 
     must_have_7_atleast_chars() {
-        assertFalse(check("h"));
-        assertFalse(check("helpme"));
-        assertTrue(check("helpme7"));
-        assertTrue(check("helpme88"));
+        assertFalse(checker.check("h"));
+        assertFalse(checker.check("helpme"));
+        assertTrue(checker.check("helpme7"));
+        assertTrue(checker.check("helpme88"));
     }
 
-    @Test public void 
+    @Test public void
     must_contain_atleast_one_digit() throws Exception {
-        assertFalse(check("helpmenow"));
+        assertFalse(checker.check("helpmenow"));
     }
     
     @Test public void 
     must_contain_atleast_one_character() throws Exception {
-        assertFalse(check("1234567"));
+        assertFalse(checker.check("1234567"));
     }
     
     @Test public void 
     can_contain_capital_letters() throws Exception {
-        assertTrue(check("WOW5OCOOL")); 
+        assertTrue(checker.check("WOW5OCOOL"));
     }
 
     
     @Test public void 
     admins_must_have_atleast_10_chars() throws Exception {
-        assertFalse(checkAdmin("helpme7"));
-        assertTrue(checkAdmin("10helpme.."));
+        String tooShort = "helpme7";
+        assertFalse(checker.checkAdmin(tooShort));
+        assertTrue(checker.checkAdmin("10helpme.."));
     }
-    
+
+
     @Test public void 
     admins_must_contain_atleast_one_digit() throws Exception {
-        assertFalse(checkAdmin("helpmenowpleas"));
+        assertFalse(checker.checkAdmin("helpmenowpleas"));
     }
     
     @Test public void 
     admins_must_contain_atleast_one_letter() throws Exception {
-        assertFalse(checkAdmin("1234567890"));
+        assertFalse(checker.checkAdmin("1234567890"));
     }
     
     @Test public void 
     admins_must_contain_atleast_one_special_char() throws Exception {
-        assertFalse(checkAdmin("heyYouWom4n"));
-        assertTrue(checkAdmin(".heyYouWom4n"));
-        assertTrue(checkAdmin("hey!YouWom4n"));
-        assertTrue(checkAdmin("hey?YouWom4n"));
-    }
-    
-    private void assertTrue(boolean condition) {
-        Assert.assertTrue(condition);
-        assertThat(checker.errors()).isEmpty();
-    }
-    
-    private void assertFalse(boolean condition) {
-        Assert.assertFalse(condition);
-        assertThat(checker.errors()).isNotEmpty();
-    }
-    
-    private boolean checkAdmin(String password) {
-        return checker.checkAdmin(password);
+        assertFalse(checker.checkAdmin("heyYouWom4n"));
+        assertTrue(checker.checkAdmin(".heyYouWom4n"));
+        assertTrue(checker.checkAdmin("hey!YouWom4n"));
+        assertTrue(checker.checkAdmin("hey?YouWom4n"));
     }
 
-    private boolean check(String password) {
-        return checker.check(password);
-    }
-    
+
 }
