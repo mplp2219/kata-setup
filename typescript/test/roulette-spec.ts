76cc138cf72067230910844af22bb7068f8f092c
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-06-15 19:36:12 +0200

    finish tests and add runners for all suites

--- typescript/test/roulette-spec.ts
+++ typescript/test/roulette-spec.ts
@@ -6,12 +6,26 @@ import {Waiter} from "../src/waiter";
 import {RouletteResult} from "../src/roulette-result";
 
 describe('Roulette', () => {
-  it('returns a random roulette result', () => {
+
+  it('creates a RouletteResult with the data from the randomizer after waiting', () => {
     let waiter = {wait: sinon.spy()};
     let randomizer = {getValue: sinon.stub().returns(0)};
     const roulette = new Roulette(waiter as Waiter, randomizer);
+
     const result = roulette.play()
+
     expect(result).eql(new RouletteResult(0))
+    sinon.assert.calledOnce(waiter.wait)
+  });
+
+  it('creates a RouletteResult with the data from the randomizer after waiting', () => {
+    let waiter = {wait: sinon.spy()};
+    let randomizer = {getValue: sinon.stub().returns(37)};
+    const roulette = new Roulette(waiter as Waiter, randomizer);
+
+    const result = roulette.play()
+
+    expect(result).eql(new RouletteResult(37))
     sinon.assert.calledOnce(waiter.wait)
   });
 
